8e747040e10feff50a507772011fe9f41b249dad
jisooyoon <yoonmallang@gmail.com>
2025-07-15 20:55:09 +0900

    ✨ 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+import Modal from 'react-modal';
+import Router from './routes/Router';
+
+Modal.setAppElement('#root'); // '#root'는 HTML에서 React가 마운트된 id
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
